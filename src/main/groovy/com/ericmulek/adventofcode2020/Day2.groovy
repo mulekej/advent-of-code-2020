fb11a757786acf15f1ea6873ef74ae0dadffc5b8
Eric Mulek <ejmulek@gmail.com>
2020-12-22 21:48:43 -0600

    day 2

--- src/main/groovy/com/ericmulek/adventofcode2020/Day2.groovy
+++ src/main/groovy/com/ericmulek/adventofcode2020/Day2.groovy
@@ -1,4 +1,81 @@
 package com.ericmulek.adventofcode2020
 
 class Day2 {
+
+    PasswordValidator passwordValidator
+
+    int countValidPasswords(List<Password> passwords) {
+        passwords.count {
+            passwordValidator.validate(it)
+        }
+    }
+}
+
+class PasswordLoader {
+
+    static final String RESOURCES_DIR = "src/main/resources"
+
+    List<Password> preProcess(String fileName) {
+        List passwords = []
+        new File("$RESOURCES_DIR/$fileName").eachLine {
+            String[] parts = it.split(' ')
+            parts[0]
+            Policy policy = getPolicy(parts[0], parts[1])
+            passwords << new Password(policies: [policy], value: parts[2])
+        }
+        passwords
+    }
+
+    private Policy getPolicy(String range, String requiredCharacterString) {
+        String requiredCharacter = requiredCharacterString.replace(':', '')
+        List<Integer> acceptedRange = range.split('-')*.toInteger()
+        new Policy(requiredCharacter: requiredCharacter, minRequired: acceptedRange[0], maxAllowed: acceptedRange[1])
+    }
+}
+
+class Password {
+
+    List<Policy> policies
+    String value
+}
+
+class Policy {
+    String requiredCharacter
+    int minRequired
+    int maxAllowed
+}
+
+interface PasswordValidator {
+
+    boolean validate(Password password)
+}
+class TobogganPasswordValidator implements PasswordValidator {
+
+    @Override
+    boolean validate(Password password) {
+        password.policies.every {Policy policy -> //every applied policy must be true
+            password.value.toList().count{
+                it == policy.requiredCharacter
+            }.with { Number matchedChars ->
+                (policy.minRequired..policy.maxAllowed).containsWithinBounds(matchedChars)
+            }
+        }
+    }
+}
+
+class RevisedPasswordValidator implements PasswordValidator {
+
+    @Override
+    boolean validate(Password password) {
+        password.policies.every {Policy policy -> //every applied policy must be true
+            List<String> characters = password.value.toList()
+            characters[policy.minRequired] == policy.requiredCharacter ||
+                    characters[policy.maxAllowed] == policy.requiredCharacter
+            /*
+            todo given the revised policy definition, minRequired and maxAllowed are misnomers,
+             code should be refactored at a later date it either be more generic or reflect the new definitions.
+             Don't have time to do it now, really need to catch my toboggan and get to the airport.
+             */
+        }
+    }
 }
--- src/test/groovy/com/ericmulek/adventofcode2020/Day2Spec.groovy
+++ src/test/groovy/com/ericmulek/adventofcode2020/Day2Spec.groovy
@@ -5,16 +5,33 @@ import spock.lang.Specification
 class Day2Spec extends Specification {
 
     Day2 systemUnderTest
+
+    List<Password> passwords
     
     void setup() {
         systemUnderTest = new Day2()
+        passwords = new PasswordLoader().preProcess('Day2Dataset.txt')
     }
     
-    void "change me"() {
+    void "Part One, count valid passwords"() {
+        given:
+        systemUnderTest.passwordValidator = new TobogganPasswordValidator()
+
         when:
-        def result = systemUnderTest
+        int result = systemUnderTest.countValidPasswords(passwords)
         
         then:
-        !result
+        result == 393
+    }
+
+    void "Part 2, use revised Policy Validator"() {
+        given:
+        systemUnderTest.passwordValidator = new RevisedPasswordValidator()
+
+        when:
+        int result = systemUnderTest.countValidPasswords(passwords)
+
+        then:
+        result == 690
     }
 }
