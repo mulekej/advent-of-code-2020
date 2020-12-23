eccabeaa3609ee6e6aa25a41a9abdbace5dbfe83
Eric Mulek <ejmulek@gmail.com>
2020-12-22 22:58:27 -0600

    day 3

--- src/main/groovy/com/ericmulek/adventofcode2020/Day3.groovy
+++ src/main/groovy/com/ericmulek/adventofcode2020/Day3.groovy
@@ -1,4 +1,52 @@
 package com.ericmulek.adventofcode2020
 
 class Day3 {
+
+    String space
+    String tree
+    List<List<String>> arealMap
+
+    Long countTreesOnRoute(Point startingPosition, Point targetPosition) {
+        Integer treesHit = 0
+        Point currentPosition = startingPosition
+        while (currentPosition.y < arealMap.size()) {
+
+            def temp = arealMap[currentPosition.y][currentPosition.x]
+            if (temp == tree) {
+                treesHit++
+            }
+            currentPosition = calculateNewPosition(currentPosition, targetPosition)
+        }
+        treesHit
+    }
+
+    private Point calculateNewPosition(Point currentPosition, Point targetPosition) {
+        new Point(x: currentPosition.x + targetPosition.x, y: currentPosition.y + targetPosition.y)
+    }
+
+    Long processMultipleRoutes(Point startingPosition, List<Point> slopes) {
+        slopes.collect {
+            countTreesOnRoute(startingPosition, it)
+        }.inject { long cumulative, long currentVal ->
+            cumulative * currentVal
+        }
+    }
+}
+
+class Point {
+    int x
+    int y
+}
+
+class Day3DataLoad {
+
+    static final String RESOURCES_DIR = "src/main/resources"
+
+    List<List<String>> process(String fileName, int repeatsToTheRight) {
+        List<List<String>> topographicMap = []
+        new File("$RESOURCES_DIR/$fileName").eachLine {
+            topographicMap << (it * repeatsToTheRight).toList()
+        }
+        topographicMap
+    }
 }
--- src/test/groovy/com/ericmulek/adventofcode2020/Day3Spec.groovy
+++ src/test/groovy/com/ericmulek/adventofcode2020/Day3Spec.groovy
@@ -7,15 +7,37 @@ class Day3Spec extends Specification {
     Day3 systemUnderTest
 
     void setup() {
-        systemUnderTest = new Day3()
+        List<List<String>> map = new Day3DataLoad().process('day3DataSet.txt', 300)
+        systemUnderTest = new Day3(arealMap: map, space: '.', tree: '#')
     }
 
-    void "change me"() {
+    void "Part 1, count number of trees hit on way down the coast"() {
+        given:
+        Point startingPosition = new Point(x: 0, y: 0)
+        Point targetPosition = new Point(x: 3, y: 1)
+
         when:
-        def result = systemUnderTest
+        int result = systemUnderTest.countTreesOnRoute(startingPosition, targetPosition)
 
         then:
-        !result
+        result == 207
     }
 
+    void "Part 2, get multiple value of trees hit given a list of slopes"() {
+        given:
+        Point startingPosition = new Point(x: 0, y: 0)
+        List<Point> slopes = [
+                new Point(x:1, y:1),
+                new Point(x:3, y:1),
+                new Point(x:5, y:1),
+                new Point(x:7, y:1),
+                new Point(x:1, y:2),
+                ]
+        when:
+        long result = systemUnderTest.processMultipleRoutes(startingPosition, slopes)
+
+        then:
+        result == 2655892800
+
+    }
 }
