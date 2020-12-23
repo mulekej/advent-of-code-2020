f621a33d18d61f688f05c356896891d982bde92b
Eric Mulek <ejmulek@gmail.com>
2020-12-22 20:30:06 -0600

    fix unit test runner. Day 1

--- src/main/groovy/com/ericmulek/adventofcode2020/Day1.groovy
+++ src/main/groovy/com/ericmulek/adventofcode2020/Day1.groovy
@@ -2,4 +2,26 @@ package com.ericmulek.adventofcode2020
 
 class Day1 {
 
+    Integer requiredSum
+
+    Integer processExpenseReportPart1(List<Integer> expenses) {
+        expenses.collect {Integer a ->
+            expenses.find {Integer b ->
+                (a + b) == requiredSum
+            }?.with { Integer b ->
+                a * b
+            }
+        }.find()
+    }
+
+    Integer processExpenseReportPart2(List<Integer> expenses) {
+        expenses.collect {Integer a ->
+            expenses.find {Integer b ->
+                (requiredSum - a - b) in expenses
+            }?.with { Integer b ->
+                Integer remainingNumber = expenses.find{ it == (requiredSum - a - b)}
+                remainingNumber * a * b
+            }
+        }.find()
+    }
 }
