05ba8f5c9abc98f18d38056fe3e4a62015bcd6ac
Eric Mulek <ejmulek@gmail.com>
2020-12-23 19:28:27 -0600

    Day 6. That one was kinda fun.

--- src/main/groovy/com/ericmulek/adventofcode2020/Day6.groovy
+++ src/main/groovy/com/ericmulek/adventofcode2020/Day6.groovy
@@ -2,4 +2,49 @@ package com.ericmulek.adventofcode2020
 
 class Day6 {
 
+    Integer countOfYesAnswers(List<List<String>> groups) {
+        groups.collect {List<String> groupAnswers ->
+            groupAnswers*.toList().flatten().unique().size()
+        }.sum() as Integer
+    }
+
+    Integer countOfAnswersPart2(List<List<String>> groups) {
+        groups.collect {List<String> groupAnswers ->
+            //get unique set of answered questions
+            groupAnswers*.toList().flatten().unique().collectEntries { String question ->
+                groupAnswers.every {
+                    it.contains(question)
+                }.with {
+                    [(question), it] //create map entry with whether the question was answered by everyone
+                }
+            }.with { Map<String, Boolean> answers ->
+                answers.count { //count how many questions were answered by everyone in the group
+                    it.value
+                }
+            }
+        }.sum() as Integer
+    }
+}
+
+class DataParser {
+
+    static final String RESOURCES_DIR = "src/main/resources"
+
+    List<List<String>> process(String fileName) {
+        List<List<String>> groupAnswers = []
+        List<String> buffer = []
+        new File("$RESOURCES_DIR/$fileName").eachLine {
+            if (it) {
+                buffer << it
+            } else {
+                groupAnswers << buffer
+                buffer = []
+            }
+        }
+        if (buffer) { //eachLine doesn't process white space at end of file
+            groupAnswers << buffer
+        }
+        groupAnswers
+    }
+
 }
--- src/test/groovy/com/ericmulek/adventofcode2020/Day6Spec.groovy
+++ src/test/groovy/com/ericmulek/adventofcode2020/Day6Spec.groovy
@@ -4,4 +4,28 @@ import spock.lang.Specification
 
 class Day6Spec extends Specification {
 
+    Day6 systemUnderTest
+
+    List<List<String>> groupAnswers
+
+    void setup() {
+        groupAnswers = new DataParser().process('day6Dataset.txt')
+        systemUnderTest = new Day6()
+    }
+
+    void "Part 1, Count number of questions answered yes per group"() {
+        when:
+        int result = systemUnderTest.countOfYesAnswers(groupAnswers)
+
+        then:
+        result == 6521
+    }
+
+    void "Part 2, Count number of questions everyone answered yes to"() {
+        when:
+        int result = systemUnderTest.countOfAnswersPart2(groupAnswers)
+
+        then:
+        result == 3305
+    }
 }
