43bf56e445e32ba583098860332baab8a35d0f4e
Eric Mulek <eric.mulek@surescripts.com>
2020-12-24 23:20:26 -0600

    day 8: broken commit, batteries dying.

--- src/main/groovy/com/ericmulek/adventofcode2020/Day8.groovy
+++ src/main/groovy/com/ericmulek/adventofcode2020/Day8.groovy
@@ -2,4 +2,59 @@ package com.ericmulek.adventofcode2020
 
 class Day8 {
 
+    List<Instruction> instructionSet
+    private Integer accumulator
+
+    private Set<Integer> executionTracker = []
+
+    Integer getValueBeforeLoop() {
+
+        Integer result
+
+        int index = 0
+        boolean proceed = true
+        while(proceed) {
+
+            def instruction = instructionSet[index]
+            switch (instruction.operation){
+                case (Operation.NOP):
+                    index++
+                    break
+                case (Operation.ACC):
+                    accumulator += instruction.argument
+
+            }
+        }
+    }
+
+}
+
+class InstructionReader {
+
+    private static final String RESOURCES_DIR = "src/main/resources"
+
+    String fileName
+
+    List<Instruction> read() {
+        new File("$RESOURCES_DIR/$fileName").readLines().collect { String line ->
+            line.replace('+', '').with { String instruction ->
+                Operation op = Operation.valueOf(instruction[0].toUpperCase())
+                Integer argument = instruction[1].toInteger()
+                new Instruction(operation:op, argument:argument)
+            }
+        }
+    }
+}
+
+class Instruction {
+
+    Operation operation
+    int argument
+}
+
+enum Operation {
+
+    ACC,
+    JMP,
+    NOP
 }
--- src/test/groovy/com/ericmulek/adventofcode2020/Day8Spec.groovy
+++ src/test/groovy/com/ericmulek/adventofcode2020/Day8Spec.groovy
@@ -4,4 +4,18 @@ import spock.lang.Specification
 
 class Day8Spec extends Specification {
 
+    Day8 systemUnderTest
+
+    void setup() {
+        List<Instruction> instructionSet = new InstructionReader().read()
+        systemUnderTest = new Day8(instructionSet:instructionSet)
+    }
+
+    void "Part 1, Find accumulator value before loop kicks in."() {
+        when:
+        int result = systemUnderTest.valueBeforeLoop
+
+        then:
+        !result
+    }
 }
